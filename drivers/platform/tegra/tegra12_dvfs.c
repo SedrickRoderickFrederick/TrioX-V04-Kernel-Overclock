4a6f33a2a2c1fcd854ef93e72d5025397a9efdd1
SedrickRoderickFrederick <167434095+SedrickRoderickFrederick@users.noreply.github.com>
2024-04-19 20:08:44 +0200

    Update tegra12_dvfs.c

--- drivers/platform/tegra/tegra12_dvfs.c
+++ drivers/platform/tegra/tegra12_dvfs.c
@@ -165,7 +165,7 @@ void __init tegra12x_vdd_cpu_align(int step_uv, int offset_uv)
 /* CPU DVFS tables */
 static unsigned long cpu_max_freq[] = {
 /* speedo_id	0	 1	  2	   3	    4	     5	      6		7	 8*/
-		2014500, 2320500, 2116500, 2524500, 1811000, 2490000, 1912500, 1912500, 2116500,
+		2014500, 2320500, 2116500, 2524500, 1811000, 2499000, 1912500, 1912500, 2116500,
 };
 
 static struct cpu_cvb_dvfs cpu_cvb_dvfs_table[] = {
@@ -781,7 +781,7 @@ static int resolve_core_override(int min_override_mv)
 /* GPU DVFS tables */
 static unsigned long gpu_max_freq[] = {
 /* speedo_id	0	1	2	 3	4	5	6*/
-		648000, 852000, 1008000, 780000, 804000, 756000, 852000
+		648000, 900000, 1008000, 780000, 804000, 756000, 900000
 };
 static struct gpu_cvb_dvfs gpu_cvb_dvfs_table[] = {
 	{
